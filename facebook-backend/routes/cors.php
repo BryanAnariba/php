2bdde5cdb3c490f51ac6b05c5511f5933f8abb92
BryanAnariba <43623601+BryanAnariba@users.noreply.github.com>
2020-07-06 20:25:25 -0600

    Creando un carrito de compras

--- facebook-backend/routes/cors.php
+++ facebook-backend/routes/cors.php
@@ -1,7 +1,7 @@
 <?php
     // Para realizar la conexion con angular o puede ser con react js tambien no importa solo cambias el puerto
     $authorizedServer = "http://localhost:4200";
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: $authorizedServer");
     header("Access-Control-Allow-Headers: content-type");
     header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
 ?>
