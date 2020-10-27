5a8ca0a781f286d229efdfed1af62edfcc244e49
Bryan Anariba <saariel115@gmail.com>
2020-10-26 18:30:23 -0600

    empezamos de nuevo

---
+++ apis-react/articles/config/cors.php
@@ -0,0 +1,8 @@
+<?php
+    // Para realizar la conexion con angular o puede ser con react js tambien no importa solo cambias el puerto
+    // Cors siempre es necesario para comunicar frontend de frameworks con nuestro backend
+    $authorizedServer = "http://localhost:3000";
+    header("Access-Control-Allow-Origin: $authorizedServer");
+    header("Access-Control-Allow-Headers: content-type");
+    header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE,PATH");
+?>
